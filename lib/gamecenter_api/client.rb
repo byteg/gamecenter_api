c4bd0b7e70f507c8e1071a4a780b4c9c381122e2
Денис Кузнецов <denisk@MacBook-Pro-Denis.local>
2021-12-28 15:26:15 +0300

    fix

--- lib/gamecenter_api/client.rb
+++ lib/gamecenter_api/client.rb
@@ -1,39 +1,41 @@
-class GamecenterApi::Client
-  attr_accessor :token, :secret_token, :base_url
-  DEFAULT_BASE_URL = "http://localhost:3000"
-  API_PATH = "api/v1"
-  
-  def initialize(token, secret_token, base_url = DEFAULT_BASE_URL)
-  	self.token, self.secret_token, self.base_url = token, secret_token, base_url
-  end
+module GamecenterApi
+  class Client
+    attr_accessor :token, :secret_token, :base_url
+    DEFAULT_BASE_URL = "http://localhost:3000"
+    API_PATH = "api/v1"
+    
+    def initialize(token, secret_token, base_url = DEFAULT_BASE_URL)
+    	self.token, self.secret_token, self.base_url = token, secret_token, base_url
+    end
 
-  def game2user(user_id, amount)
-  	send_post "#{base_url}/#{API_PATH}/transfers/game2user", { user_id: user_id, amount: amount }
-  end
+    def game2user(user_id, amount)
+    	send_post "#{base_url}/#{API_PATH}/transfers/game2user", { user_id: user_id, amount: amount }
+    end
 
-  def user2game(user_id, amount)
-  	send_post "#{base_url}/#{API_PATH}/transfers/user2game", { user_id: user_id, amount: amount }
-  end
+    def user2game(user_id, amount)
+    	send_post "#{base_url}/#{API_PATH}/transfers/user2game", { user_id: user_id, amount: amount }
+    end
 
-  def game_info
-  	send_get "#{base_url}/#{API_PATH}/game"
-  end
+    def game_info
+    	send_get "#{base_url}/#{API_PATH}/game"
+    end
 
-  def send_notification(user_id, content)
-  	send_post "#{base_url}/#{API_PATH}/notifications", { user_id: user_id, content: content }
-  end
+    def send_notification(user_id, content)
+    	send_post "#{base_url}/#{API_PATH}/notifications", { user_id: user_id, content: content }
+    end
 
-  private
+    private
 
-  def send_post(url, params = {})
-  	params.merge!({ token: token, secret_token: secret_token })
-  	response = RestClient.post url, params
-  	JSON.parse(response.body)
-  end
+    def send_post(url, params = {})
+    	params.merge!({ token: token, secret_token: secret_token })
+    	response = RestClient.post url, params
+    	JSON.parse(response.body)
+    end
 
-  def send_get(url, params = {})
-  	params.merge!({ token: token, secret_token: secret_token })
-  	response = RestClient.get url, params: params
-  	JSON.parse(response.body)
+    def send_get(url, params = {})
+    	params.merge!({ token: token, secret_token: secret_token })
+    	response = RestClient.get url, params: params
+    	JSON.parse(response.body)
+    end
   end
 end
